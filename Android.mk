cb37fd082681baa086ecde46bb98c57b05e36e47
Vitaliy Tomin <highwaystar.ru@gmail.com>
2022-05-28 01:51:46 +0800

    Guard for s4, t7 SOCs
    Change-Id: Ic900996019110fe5f05c9865efe04950eac8ae80

--- Android.mk
+++ Android.mk
@@ -14,6 +14,7 @@
 # limitations under the License.
 #
 
+ifneq ($(filter s4 t7,$(TARGET_AMLOGIC_SOC)),)
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
@@ -67,4 +68,5 @@ $(_media_ko): $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/$(BOARD_KERNEL_IMAGE_NAME)
 	touch $(_media_intermediates)/media.ko
 
 include $(BUILD_SYSTEM)/base_rules.mk
+endif
 endif
